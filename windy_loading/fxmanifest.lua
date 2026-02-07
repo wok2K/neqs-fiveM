f9eb5043f753a774db9891a676be5a11ab766da5
wok2K <sakiwtf@gmail.com>
2026-02-06 21:26:52 -0400

    Fix video playback: use local assets and bundle jQuery

---
+++ windy_loading/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+version '1.0.0'
+
+loadscreen 'nui/index.html'
+loadscreen_cursor 'yes'
+lua54 'yes'
+
+files {
+    'nui/index.html',
+    'nui/js/app.js',
+    'nui/js/jquery.min.js',
+    'nui/js/musicplayer.js',
+    'config/config.js',
+    'nui/css/style.css',
+    'nui/assets/music.mp3',
+    'nui/assets/BIG 3 bloodhound lil jeff.mp3',
+    'nui/assets/Did it First ice spice.mp3',
+    'nui/assets/When You call - heembeezy.mp3',
+    'nui/assets/Bloodhound_Lil_Jeff_x_Bloodhound_Q50_x_Slimeto_-_BIG_3_Official_Video_Dir._Yardiefilms_-_Yardie_Films_Production_1080p_h264.mp4',
+    'nui/assets/Ice_Spice_Central_Cee_-_Did_It_First_-_IceSpiceVEVO_1080p_h264.mp4',
+    'nui/assets/8K_x_Heembeezy_-_When_You_Call_Official_Music_Video_Dir._by_Kevin_Mora_-_Moraculous_1080p_h264.mp4',
+    'nui/assets/BIG 3LLJEFF.png',
+    'nui/assets/icespize.png',
+    'nui/assets/heembeezy.png',
+    'nui/font/TTLakesNeue-Black.ttf',
+    'nui/font/TTLakesNeue-Bold.ttf',
+    'nui/font/TTLakesNeue-Medium.ttf'
+}
+dependency '/assetpacks'
